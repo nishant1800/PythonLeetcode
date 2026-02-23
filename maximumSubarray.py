e7518061e653d2e959ef390aa1fb5ceef9b29a86
NishantRanjan <ranjannishant18011@gmail.com>
2026-02-23 21:48:45 +0530

    leetcode 53

---
+++ maximumSubarray.py
@@ -0,0 +1,17 @@
+# 53. Maximum Subarray
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        currSum = 0
+        maxSum = nums[0]
+
+        for i in range(len(nums)):
+            currSum += nums[i]
+
+            if currSum > maxSum:
+                maxSum = currSum
+
+            if currSum < 0:
+                currSum = 0
+
+        return maxSum
